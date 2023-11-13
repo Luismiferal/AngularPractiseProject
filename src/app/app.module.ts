d37860729bb0002d70130bbbe99fdb77e5a2897c
Luismiferal <Luismiferal@gmail.com>
2023-11-12 20:02:30 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { PagesModule } from './pages/pages.module';
+import { StudentModule } from './student/student.module';
+import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    PagesModule,
+    StudentModule,
+    AuthModule
+
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
