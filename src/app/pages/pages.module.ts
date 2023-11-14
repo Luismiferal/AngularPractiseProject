72621057f47a0211ec2d8fdaf7a40d46b0c050b6
Luismiferal <Luismiferal@gmail.com>
2023-11-14 16:09:58 -0400

    Creation fo Routes and Login

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,6 +8,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav'; 
 import {MatDividerModule} from '@angular/material/divider'; 
 import {MatButtonModule} from '@angular/material/button'; 
+import { RouterModule } from '@angular/router';
 
 
 
@@ -18,6 +19,7 @@ import {MatButtonModule} from '@angular/material/button';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     MatInputModule,
     MatToolbarModule,
     MatIconModule,
